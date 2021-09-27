0104142232e84a7836ffcecdde73f660eb0e049f
ESTEBAN <esteban.v.web@gmail.com>
2021-09-26 19:54:12 -0500

    implement default and cart routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './catalog/products/products.component';
+import { CartComponent } from './checkout/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
