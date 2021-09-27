f727c67e435ab609f07c1562e557f2bde831999d
ESTEBAN <esteban.v.web@gmail.com>
2021-09-26 21:48:40 -0500

    implement catalog product route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './catalog/products/products.component';
 import { CartComponent } from './checkout/cart/cart.component';
+import { ProductComponent } from './catalog/product/product.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'product/:id', component: ProductComponent },
 ];
 
 @NgModule({
--- src/app/catalog/product/product.component.ts
+++ src/app/catalog/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  productId = undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.productId = params.id;
+    })
   }
 
 }
